fb7e663b8cb32aa6ecdb76c5784f748bd8e3ae9c
Martin Vasseur <martin.vasseur@le-campus-numerique.fr>
2023-06-15 17:06:13 +0200

    Site e-commerce passage en PDO avec affichage du catalogue qui marche

--- catalogue.php
+++ catalogue.php
@@ -1,4 +1,6 @@
 <?php
+include "./template/header.php";
+include "my-functions.php";
 
 $db = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'Martin3899', 'Actutennis38!');
 $stmt= $db->prepare('select * from products ');
@@ -9,8 +11,8 @@ $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $catalogue=new Catalogue($products);
 $catalogue->displayCatalogue($products);
 class Item{
-
-    private string $name;
+    public int $id;
+    public string $name;
     public string $description;
     public int $price;
     public string $imageURL;
@@ -18,13 +20,14 @@ class Item{
     public int $stock;
     public bool $available;
 
-   public function __constructor($name, $price,  $imageURL, $weight ): void
+   public function __construct(int $id,string $name, int $price,  string $imageURL, int $weight, int $stock)
     {
+        $this->id=$id;
         $this->name=$name;
         $this->price=$price;
         $this->imageURL=$imageURL;
         $this->weight=$weight;
-
+        $this->stock=$stock;
     }
 
     /**
@@ -37,10 +40,27 @@ class Item{
 
     public function displayItem(){
         ?>
-        <h3> <?php echo $this->getName() ?> </h3>
-        <img src="<?php echo $this->imageURL ?>" height="400" width="400" alt="image vélo">
-        <p><?php echo $this->price ?></p>
-        <p><?php echo $this->weight ?></p>
+        <div class="container">
+            <div class="columns">
+                <div class="columns__column">
+                    <img src="<?php echo $this->imageURL ?>" height="400" width="400" alt="image vélo"/>
+                </div>
+                <div class="columns__column">
+                    <h3><?=  $this->getName()?></h3>
+                    <p>Prix : <?= formatPrice($this->price)  ?></p>
+                    <p><?= "Poids: " . formatWeight($this->weight)  ?></p>
+
+                    <form class="form" action="cart.php" method="post">
+                        <input type="hidden" name="action" value="add">
+                        <input type="hidden" id="marqueVelo" name="product" value="<?= $this->id?>">
+                        <label for="nombreVelo">Quantité</label>
+                        <input class="quantiy_input" id="nombreVelo" type="number" name="quantity" value="0" min="0", max="<?= $this->stock ?>">
+                        <p><input class="submit_button" type="submit" name="order_form" value="Ajouter au panier"> </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+
         <?php
     }
 
@@ -57,12 +77,13 @@ class Catalogue{
    public function displayCatalogue($products){
 
         foreach ($products as $item){
-            $cat = new Item($item['name'],$item['price'], $item['picture'], $item['weight']);
+            $cat = new Item($item['id'],$item['name'],$item['price'], $item['picture'],$item['weight'],$item['stock']);
             $cat->displayItem();
         }
     }
 }
 
+include "./template/footer.php";
 
 
 //$giant = new Item();
